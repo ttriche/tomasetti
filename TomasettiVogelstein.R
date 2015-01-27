97506a6f9ab4927821b0cab22653cd5b8e9a0157
ttriche <tim.triche@gmail.com>
2015-01-26 19:29:16 -0800

    remove superfluous code

--- TomasettiVogelstein.R
+++ TomasettiVogelstein.R
@@ -3,9 +3,7 @@ TyV <- TyV[, c(1,6)]
 names(TyV) <- c('risk','lscd')
 
 R <- with(TyV, cor(risk, lscd))
-R2 <- with(TyV, cor(risk, lscd))**2
-print(paste("Pearson's R is", R, "and R^2 is", R2, "on the raw data."))
+print(paste("Pearson's R is", R, "and R^2 is", R**2, "on the raw data."))
 
 Rlog10 <- with(TyV, cor(log10(risk), log10(lscd)))
-R2log10 <- with(TyV, cor(log10(risk), log10(lscd)))**2
-print(paste("Pearson's R is", Rlog10, "and R^2 is", R2log10, "on log10(data)."))
+print(paste("Pearson's R is",Rlog10,"and R^2 is",Rlog10**2,"on log10(data)."))
